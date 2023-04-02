e23b19773e64d6d4cf9b5ab138a556a04e1b7233
SumanMondalCSE <smondal.qwerty@gmail.com>
2023-04-02 16:15:16 +0530

    car border interaction

--- pygame/main/main.py
+++ pygame/main/main.py
@@ -5,6 +5,10 @@ from math import sin, cos, pi, atan2
 #
 pygame.init()
 
+# screen
+width = 1200
+height = 600
+
 # Caption and Icon
 pygame.display.set_caption("Invader")
 icon = pygame.image.load('pygame/resource/logo.png')
@@ -16,8 +20,8 @@ playerImg_right = pygame.image.load('pygame/resource/player_right.png')
 playerImg_left = pygame.image.load('pygame/resource/player_left.png')
 playerImg0 = playerImg_straight
 rect0_player = playerImg0.get_rect()
-playerX = 400
-playerY = 300
+playerX = width/2
+playerY = height/2
 rect0_player.center = (playerX, playerY)
 angle_player = 0
 angle_player_r = 0
@@ -34,17 +38,9 @@ def player():
 
     global playerX
     global playerY
+    global angle_player
     global angle_gun
     angle_gun += 1
-
-    if playerX <= 70:
-        playerX = 70
-    elif playerX >= 730:
-        playerX = 730
-    if playerY <= 70:
-        playerY = 70
-    elif playerY >= 530:
-        playerY = 530
 
     rect0_player.center = (playerX, playerY)
     playerImg1 = pygame.transform.rotate(playerImg0, angle_player)
@@ -54,7 +50,7 @@ def player():
     xdist = enemy_cords[0] - rect0_player.centerx
     ydist = enemy_cords[1] - rect0_player.centery
 
-    angle_gun =  atan2(xdist, ydist) * 180 / pi + 180    
+    angle_gun = atan2(xdist, ydist) * 180 / pi + 180
     angle_gun %= 360
 
     playerImg_gun1 = pygame.transform.rotate(playerImg_gun0, angle_gun)
@@ -70,7 +66,7 @@ seed = random.randint(0, 9)
 chunk_coords = [0, 0]
 
 # create screen
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((width, height))
 
 running = True
 # update_chunk(coords)
@@ -111,17 +107,105 @@ while running:
         playerX += 1.8 * sin(angle_player_r)
 
     # player image
-    if keys[pygame.K_UP] or keys[pygame.K_w]:
-        playerImg0 = playerImg_straight
-
-    if keys[pygame.K_DOWN] or keys[pygame.K_s]:
-        playerImg0 = playerImg_straight
-
     if keys[pygame.K_LEFT] or keys[pygame.K_a]:
         playerImg0 = playerImg_left
 
-    if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+    elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
         playerImg0 = playerImg_right
+
+    elif keys[pygame.K_UP] or keys[pygame.K_w] or keys[pygame.K_DOWN] or keys[pygame.K_s]:
+        playerImg0 = playerImg_straight
+
+    if playerX < 70:
+        playerX = 70
+        if angle_player < 70:
+            angle_player -= (70-angle_player)/50
+        elif angle_player < 90:
+            angle_player -= (70-angle_player)/50
+
+        elif angle_player < 110:
+            angle_player -= (110-angle_player)/50
+        elif angle_player < 180:
+            angle_player -= (110-angle_player)/50
+
+        elif angle_player < 250:
+            angle_player -= (250-angle_player)/50
+        elif angle_player < 270:
+            angle_player -= (250-angle_player)/50
+
+        elif angle_player < 290:
+            angle_player -= (290-angle_player)/50
+        else:
+            angle_player -= (290-angle_player)/50
+
+    elif playerX > 1130:
+        playerX = 1130
+        if angle_player < 70:
+            angle_player -= (70-angle_player)/50
+        elif angle_player < 90:
+            angle_player -= (70-angle_player)/50
+
+        elif angle_player < 110:
+            angle_player -= (110-angle_player)/50
+        elif angle_player < 180:
+            angle_player -= (110-angle_player)/50
+
+        elif angle_player < 250:
+            angle_player -= (250-angle_player)/50
+        elif angle_player < 270:
+            angle_player -= (250-angle_player)/50
+
+        elif angle_player < 290:
+            angle_player -= (290-angle_player)/50
+        else:
+            angle_player -= (290-angle_player)/50
+
+
+    if playerY < 70:
+        playerY = 70
+        if angle_player < 20:
+            angle_player -= (20-angle_player)/50
+        elif angle_player < 90:
+            angle_player -= (20-angle_player)/50
+
+        elif angle_player < 160:
+            angle_player -= (160-angle_player)/50
+        elif angle_player < 180:
+            angle_player -= (160-angle_player)/50
+
+        elif angle_player < 200:
+            angle_player -= (200-angle_player)/50
+        elif angle_player < 270:
+            angle_player -= (200-angle_player)/50
+
+        elif angle_player < 340:
+            angle_player -= (340-angle_player)/50
+        else:
+            angle_player -= (340-angle_player)/50
+
+
+    elif playerY > 530:
+        playerY = 530
+        if angle_player < 20:
+            angle_player -= (20-angle_player)/50
+        elif angle_player < 90:
+            angle_player -= (20-angle_player)/50
+
+        elif angle_player < 160:
+            angle_player -= (160-angle_player)/50
+        elif angle_player < 180:
+            angle_player -= (160-angle_player)/50
+
+        elif angle_player < 200:
+            angle_player -= (200-angle_player)/50
+        elif angle_player < 270:
+            angle_player -= (200-angle_player)/50
+
+        elif angle_player < 340:
+            angle_player -= (340-angle_player)/50
+        else:
+            angle_player -= (340-angle_player)/50
+
 
     player()
 
